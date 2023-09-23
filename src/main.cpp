0a7cda490523da77ea774d488d77f59c7f588b97
Tomáš Diblík <dibla.tomas@post.cz>
2023-09-23 14:53:38 +0200

    [Init]

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#define DEBUG true
+#define READ_125KHz_BUTTON 2
+#define READ_13_56MHz_BUTTON 4
+SoftwareSerial rfid_serial = SoftwareSerial(10, 11); // RX,TX POV of the device, meaning you should connect TX to 5 and RX to 6
+
+void debug_println(const char* msg) {
+#ifdef DEBUG
+  Serial.println(msg);
+#endif
+}
+
+void setup() {
+#ifdef DEBUG
+  Serial.begin(9600);
+  debug_println("Starting...");
+#endif
+  rfid_serial.begin(115000);
+
+  pinMode(READ_125KHz_BUTTON, INPUT);
+  pinMode(READ_13_56MHz_BUTTON, INPUT);
+}
+
+void loop() {
+  if (digitalRead(READ_125KHz_BUTTON) == HIGH) {
+    debug_println("Reading 125KHz...");
+  }
+  if (digitalRead(READ_13_56MHz_BUTTON) == HIGH) {
+    debug_println("Reading 13.56MHz...");
+  }
+  delay(5000);
+}
