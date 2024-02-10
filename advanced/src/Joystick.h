d2f3276163dc6a219ec371dc94f3f484af389d49
Tomáš Diblík <dibla.tomas@post.cz>
2024-02-10 23:49:59 +0100

    [Fix] Change X and D pins because I soldered it incorrectlly xdd

--- advanced/src/Joystick.h
+++ advanced/src/Joystick.h
@@ -12,15 +12,15 @@ enum JoystickState {
 class Joystick {
 private:
     const uint8_t _power_pin;
-    const uint8_t _d_pin;
     const uint8_t _x_pin;
+    const uint8_t _d_pin;
 
 public:
     static const uint16_t DOWN_TRIGGER_THRESHOLD = 10; // 0 to DOWN_TRIGGER_THRESHOLD -> press triggered
     static const uint16_t Y_UP_THRESHOLD = 300; // 0 to Y_UP_THRESHOLD -> Pointing UP
     static const uint16_t Y_DOWN_THRESHOLD = 700; // Y_DOWN_THRESHOLD to 1024 -> Pointing DOWN
 
-    Joystick(uint8_t power_pin, uint8_t d_pin, uint8_t x_pin) : _power_pin(power_pin), _d_pin(d_pin), _x_pin(x_pin) {};
+    Joystick(uint8_t power_pin, uint8_t x_pin, uint8_t d_pin) : _power_pin(power_pin), _x_pin(x_pin), _d_pin(d_pin) {};
     void setup();
     JoystickState get_status();
 };
